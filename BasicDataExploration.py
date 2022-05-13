9485c2a6dc369de24c3beb0f850a7e0930454c80
BarbosaFelipeB <barbosafelipeb@gmail.com>
2022-05-12 23:11:53 -0300

    Add Housing Prices notebook

--- BasicDataExploration.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-melbourn_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
-melbourn_data = pd.read_csv(melbourn_file_path)
-melbourn_data.describe()
--- Building Your Model.py
+++
@@ -1,6 +0,0 @@
-# Building Your Model
-
-from sklearn.tree import DecisionTreeRegressor
-# Define model. Specify a number for random_state to ensure same results each run
-melbourne_model = DecisionTreeRegressor(random_state=1)
-melbourne_model.fit(X, y)
